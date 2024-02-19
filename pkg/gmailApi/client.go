1affeaf32eafcdb93f79e9be17b513cb0f7199c3
Natalie Pentschev <npentschev@berkeley.edu>
2024-02-19 15:09:22 -0800

    imported necessary packages for the gmailAPI client and openaiAPI client and defined thier structs

--- pkg/gmailApi/client.go
+++ pkg/gmailApi/client.go
@@ -1,1 +1,7 @@
 // Setup and authenticate GMAIL API client
+package gmailapi
+
+//GmailClient represents a client to connect with Gmail API
+type GmailClient struct {
+	service *gmail.Service
+}
--- pkg/openaiApi/client.go
+++ pkg/openaiApi/client.go
@@ -1,1 +1,12 @@
 // Setup and authenticate OpenAI API client
+package openaiapi
+
+import (
+	"net/http"
+)
+
+// OpenAIClient represents a client to connect with OpenAI API
+type OpenAIClient struct {
+	httpClient *http.Client
+	apiKey     string
+}

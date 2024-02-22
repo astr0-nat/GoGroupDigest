89bf2b85317d43c608a1d26f6a22b9eecc46659b
Natalie Pentschev <npentschev@berkeley.edu>
2024-02-21 17:13:37 -0800

    skeleton code for gmailApi/client.go

--- pkg/gmailApi/client.go
+++ pkg/gmailApi/client.go
@@ -1,7 +1,33 @@
 // Setup and authenticate GMAIL API client
 package gmailapi
 
-//GmailClient represents a client to connect with Gmail API
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/gmail/v1"
+
+// GmailClient struct holds the necessary details fo the Gmail client
 type GmailClient struct {
 	service *gmail.Service
 }
+
+// NewGmailClient initializes a new Gmail client with the necessary credentials
+func NewGmailClient(ctx context.Context, config *oauth2.Config) *GmailClient {
+	// TODO: Implement a token retrieval and create an http.Client with the token
+	// client := config.Client(ctx, token)
+
+	srv, err := gmail.New(client)
+	if err != nil {
+		log.Fatalf("Unable to create a Gmail client: %v", err)
+	}
+
+	return &GmailClient{
+		Service: srv,
+	}
+}
+
+//GmailClient represents a client to connect with Gmail API
